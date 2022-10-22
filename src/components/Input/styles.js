6b95db9c6b0533c13717dca45102641bbf19191c
joonmarion <marianojoao52@gmail.com>
2022-10-22 11:35:53 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #aaaaff;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #aaaaff;
+        color: white;
+        border: 0;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        text-align: right;
+    }
+`;
